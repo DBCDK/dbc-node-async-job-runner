f5dac4c44f2c45795d5b68f44cb1b248e6e62c63
Jonas Peter Hyatt <jph@dbc.dk>
2016-06-18 12:10:45 +0200

    enable travis CI

--- src/__tests__/dummy.test.js
+++ src/__tests__/dummy.test.js
@@ -1,13 +1,15 @@
+/* global it, describe */
+
 /**
  * @file: This file contains tests
  */
 
-const expect = require('expect');
+const expect = require('expect')
 
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      expect([1,2,3].indexOf(5)).toEqual(-1);
-    });
-  });
-});
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      expect([1, 2, 3].indexOf(5)).toEqual(-1)
+    })
+  })
+})
