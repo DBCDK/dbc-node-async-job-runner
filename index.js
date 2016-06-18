ef7197a55f6a22d5c3e41c785813842d404db9d2
Jonas Peter Hyatt <jph@dbc.dk>
2016-06-18 12:00:19 +0200

    enable travis CI

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-const koa = require('koa');
-const app = koa();
-const PORT = 1234;
+const koa = require('koa')
+const app = koa()
+const PORT = 1234
 
-app.use(function *() {
-  this.body = 'Hello Automated World!';
-});
+app.use(function * () {
+  this.body = 'Hello Automated World!'
+})
 
 app.listen(PORT, () => {
-  console.log(`Started listening on ${PORT}`);
-});
+  console.log(`Started listening on ${PORT}`)
+})
